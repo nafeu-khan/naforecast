3285e9ac078ed2598fe970e3d6fd0f2a8e514584
nafeu-khan <nafeu.khan@gmail.com>
2023-07-15 02:30:51 +0600

    error msz

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import {api} from  "./keys"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 // import "bootstrap/dist/css/bootstrap.min.css"
 function App() {
@@ -8,32 +10,37 @@ function App() {
   const search=(e)=>{
     if(e.key==="Enter"){
       fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api.key}`)
-      .then((r)=> { 
+      .then((r) => { 
+        if (!r.ok) {
+          const msz = "Error! Can't fetch latitude and longitude for the city";
+          throw new Error(msz);
+        }
         return r.json(); 
       })
-      .then((res)=> {
-        console.log(res)
-        console.log(res[0].lat)
-        console.log(res[0].lon)
+      .then((res) => {
         return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${res[0].lat}&lon=${res[0].lon}&appid=${api.key}`); 
       })
-      .then((res)=> {
+      .then((res) => {
+        if (!res.ok) {
+          const msz = "Error! Can't fetch weather for the city at this time";
+          throw new Error(msz);
+        }
         return res.json(); 
       })
-      .then((result)=>{
+      .then((result) => {
         setCity("");
         setWeather(result);
       })
+      .catch((error) => {
+        toast.error(error.message); // Display error message using React-Toastify
+      });    
     }
   }
-  console.log(weather)
-  console.log(city)
   return (
     <div className=
-        {
-          (typeof weather.main!="undefined"?
+        {(typeof weather.main!="undefined"?
         ((weather.main.temp-273>18)?"hot":"cold"):"App")
-        } >
+        }>
       <main>
         <div className='logo-holder'>
           <div className='logo-1'>
@@ -63,7 +70,9 @@ function App() {
              <div className='weather'>{weather.weather[0].main}</div>
             </div>
             </div>
-        ):(<div></div>)
+        ):( <div>
+          <ToastContainer /> {/* Add this line to display the ToastContainer */}
+        </div>)
         }
       </main>
     </div>
